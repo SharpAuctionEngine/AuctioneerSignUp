52d38e7dc71ad1fe23ae5e6ebf9562610c9ad69a
InstanceOfMichael <michael@spacegazebo.com>
2015-12-28 16:23:56 -0600

    cleanup

--- server.js
+++ server.js
@@ -7,14 +7,10 @@ var app = express();
 var bodyParser = require('body-parser');
 var mandrill_api = require('mandrill-api/mandrill');
 var mandrill = new mandrill_api.Mandrill(process.env.MANDRILL_KEY);
-// var pg = require('pg');
 var fs = require('fs');
-// var connectionString = process.env.DATABASE_URL || 'postgres://postgres:password@localhost:5432/auctioneersignup';
-// var client = new pg.Client(connectionString);
 var Sequelize = require('sequelize');
 var rp = require('request-promise');
 var db = new Sequelize(process.env.CONNECTION);
-// client.connect();
 var Promise = require("es6-promise").Promise;
 var stripeAPI = require("stripe")("sk_test_qNt8nbmpti7cUDTSpSwrQoQJ");
 var subscribeToStripe = require('./lib/subscribeToStripe');
@@ -25,14 +21,12 @@ var getStripePlans = require('./lib/getStripePlans')(stripeAPI);
 var dumpPromise = require('./lib/debug/dumpPromise');
 var sendNewRequestEmailToStaff = require('./lib/sendNewRequestEmailToStaff')(mandrill);
 
-// const SEND_EMAIL_TO = require('./lib/getSendEmailTo')(process.env.SEND_EMAIL_TO);
-// console.log({SEND_EMAIL_TO:SEND_EMAIL_TO});
-
 var renderIndexHtmlOnStartUp = require('./lib/renderIndexHtmlOnStartUp')();
 
 renderIndexHtmlOnStartUp(getStripePlans());
 
 app.use(bodyParser());
+
 var server = app.listen(process.env.APP_PORT, function() {
   console.log('Listing on port: ' + process.env.APP_PORT);
 });
