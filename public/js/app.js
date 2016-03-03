b6d511020f9d3092f4fd835472913be7cde1e374
karthick92 <karthick@sae.bid>
2016-03-02 20:27:32 -0600

    #78,Language Fixed

--- public/js/app.js
+++ public/js/app.js
@@ -665,7 +665,7 @@ var ajaxCallToAp = $.debounce(450,function($data,validate_field)
 
                     
 
-                    bootbox.alert('There are validation errors.Please click on previous button to review errors');
+                    bootbox.alert('There are validation errors. Please click on previous button to review errors');
 
                     return ;
                     }
